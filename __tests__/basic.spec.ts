e2f77cea1dabdfadd9a0034691c7125adbf05a18
PlayMa256 <matheus.g.silva@hotmail.com>
2020-05-30 12:53:38 -0300

    add basic tests

---
+++ __tests__/basic.spec.ts
@@ -0,0 +1,5 @@
+describe('should run test', () => {
+  it('should pass', () => {
+    expect(1).toEqual(1);
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   notify: true,
-  testPathIgnorePatterns: ['/__tests__/.*/__fixtures__/.*'],
   collectCoverageFrom: ['index.js', 'src/**/*.{js,ts}'],
-  testMatch: ['**/*.test.js']
+  testMatch: ['**/*.spec.ts']
 };
