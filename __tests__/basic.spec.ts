4770a01d78d595c3e8e1232a55cb971fde967416
Matheus Gonçalves da Silva <831308+PlayMa256@users.noreply.github.com>
2020-05-30 13:19:08 -0300

    feat(lerna): add lerna for multi subprojects (#12)

--- __tests__/basic.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('should run test', () => {
-  it('should pass', () => {
-    expect(1).toEqual(1);
-  })
-})
---
+++ packages/cli/tests/basic.spec.ts
@@ -0,0 +1,32 @@
+import { analyze } from '../src';
+import path from 'path';
+
+const fixturePath = path.resolve(__dirname, '../test-node-modules');
+
+describe('should run test', () => {
+  it('should scan the whole tree without breaking anything and showing everything', () => {
+    const results = analyze(fixturePath);
+    const expectedResult = {
+      "@babel/helpers1": {
+        "problems": [
+          "Jenkinsfile"
+        ],
+        "saved": "6.2 MB"
+      },
+      "@babel/helpers2": {
+        "problems": [
+          "Jenkinsfile"
+        ],
+        "saved": "6.2 MB"
+      },
+      "other-dep": {
+        "problems": [
+          "AUTHORS"
+        ],
+        "saved": "6.2 MB"
+      }
+    };
+    expect(results.perPackage).toEqual(expectedResult);
+    expect(results.totalSaved).toEqual(19509168);
+  })
+})
