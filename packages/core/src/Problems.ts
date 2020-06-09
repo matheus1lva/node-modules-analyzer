1dd33c03eb39be264d4239e90bd66248fa30e7f0
PlayMa256 <matheus.g.silva@hotmail.com>
2020-06-08 22:01:25 -0300

    fix problems ffor search

--- packages/core/src/Problems.ts
+++ packages/core/src/Problems.ts
@@ -1,41 +1,47 @@
-import { hasKey } from "./utils";
-import { getFolderSize } from "./sizeUtils";
-import blacklisted from "./blacklisted";
+import { hasKey } from './utils';
+import { getFolderSize } from './sizeUtils';
+import blacklisted from './blacklisted';
+import { lstatSync } from 'fs';
+import { getSubDirectories } from './index';
 
 export class Problems {
   constructor(readonly paths: string[]) {
-    this.scan();
+    this.scan(this.paths);
   }
 
   report = {
     problems: [],
     totalSize: 0
-  }
+  };
 
-	scanSrcFolder(packageJson, path: string) {
-    if(packageJson && path.includes('src')) {
+  scanSrcFolder(packageJson, path: string) {
+    if (packageJson && path.includes('src')) {
       const mainField = packageJson?.main;
       const devDeps = packageJson?.devDependencies;
       const hasFlow = hasKey(devDeps, 'flow-bin');
 
       // edge case for flow binaries, where the don't have any way
       // to get the type definitions as ts has
-      if(mainField?.includes('dist/') && !hasFlow){
-        this.report.problems = [...this.report.problems, "src"];
+      if (mainField?.includes('dist/') && !hasFlow) {
+        this.report.problems = [...this.report.problems, 'src'];
         this.report.totalSize += getFolderSize(path);
       }
     }
   }
 
-  scan() {
-    this.paths.forEach((dir: string) => {
+  scan(paths: Array<string>) {
+    if (!paths || !paths.length) {
+      return;
+    }
+
+    paths.forEach((dir: string) => {
       const splittedFullPath = dir.split('/');
       const dirName = splittedFullPath.pop();
       const fullpathWithoutFilename = splittedFullPath.join('/');
       let packageJson = null;
       try {
         packageJson = require(`${fullpathWithoutFilename}/package.json`);
-      }catch (err) {
+      } catch (err) {
         // do nothing
       }
 
@@ -52,7 +58,13 @@ export class Problems {
             return itemBlacklisted;
           }
         });
-      this.report.problems = [ ...this.report.problems, ...problemsFound ];
+
+      this.report.problems = [...this.report.problems, ...problemsFound];
+
+      if (lstatSync(dir).isDirectory() && !paths.includes('src')) {
+        const folderContent = getSubDirectories(dir);
+        this.scan(folderContent);
+      }
     });
   }
 }
